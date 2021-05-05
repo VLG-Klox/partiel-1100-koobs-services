8b633234a9f70164474482c376a0e44e3131f02f
Romain Quellec <romain.quellec@gmail.com>
2021-05-05 16:24:37 +0200

    Payment service update

--- src/webhook.ts
+++ src/webhook.ts
@@ -8,46 +8,47 @@ const stripe = new Stripe(PAYMENT_SECRET as string, {
 	apiVersion: '2020-08-27',
 });
 
-const webhook = (
-	req: express.Request
-): Promise<void> =>
+const webhook = (req: express.Request): Promise<void> =>
 	(async () => {
 		try {
 			const { data, eventType } = req.query;
+			const signature = req.headers['stripe-signature'];
 
 			// Check if webhook signing is configured.
-			if (process.env.WEBHOOK_SECRET) {
-			  // Retrieve the event by verifying the signature using the raw body and secret.
-			  let event;
-			  let signature = req.headers["stripe-signature"];
-			  try {
-				event = stripe.webhooks.constructEvent(
-				  req.rawBody,
-				  signature,
-				  process.env.WEBHOOK_SECRET
-				);
-			  } catch (err) {
-				console.log(`⚠️  Webhook signature verification failed.`);
-				
-			  }
-			  data = event.data;
-			  eventType = event.type;
+			if (WEBHOOK_SECRET) {
+				// Retrieve the event by verifying the signature using the raw body and secret.
+				let event;
+
+				try {
+					event = stripe.webhooks.constructEvent(
+						// @ts-ignore
+						req.rawBody,
+						// @ts-ignore
+						signature,
+						WEBHOOK_SECRET
+					);
+				} catch (err) {
+					console.log(`⚠️  Webhook signature verification failed.`);
+				}
+
+				//const data = event.data;
+				//const eventType = event.type;
 			} else {
-			  // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-			  // we can retrieve the event data directly from the request body.
-			  data = req.body.data;
-			  eventType = req.body.type;
+				console.log(` secret is not configured in config.js`);
 			}
-		  
-			if (eventType === "payment_intent.succeeded") {
-			  // Funds have been captured
-			  // Fulfill any orders, e-mail receipts, etc
-			  // To cancel the payment you will need to issue a Refund (https://stripe.com/docs/api/refunds)
-			  console.log("💰 Payment received!");
-			} else if (eventType === "payment_intent.payment_failed") {
-			  console.log("❌ Payment failed.");
+
+			if (eventType === 'payment_intent.succeeded') {
+				// Funds have been captured
+				// Fulfill any orders, e-mail receipts, etc
+				// To cancel the payment you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+				console.log('💰 Payment received!');
+			} else if (eventType === 'payment_intent.payment_failed') {
+				console.log('❌ Payment failed.');
 			}
-			
+		} catch (error) {
+			console.error(error);
+			return error;
+		}
 	})();
 
-export default createCustomer;
+export default webhook;
